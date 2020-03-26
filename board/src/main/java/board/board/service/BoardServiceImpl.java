852f29302cd07738bffbdfac0e9c440e3e5ec162
nampole <nampole@aol.com>
2020-03-26 17:00:36 +0900

    chapter 6 completed

--- board/src/main/java/board/board/service/BoardServiceImpl.java
+++ board/src/main/java/board/board/service/BoardServiceImpl.java
@@ -4,15 +4,24 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import board.board.dto.BoardDto;
+import board.board.dto.BoardFileDto;
 import board.board.mapper.BoardMapper;
+import board.common.FileUtils;
 
 @Service
+//@Transactional
 public class BoardServiceImpl implements BoardService{
 	
 	@Autowired
 	private BoardMapper boardMapper;
+	
+	@Autowired
+	private FileUtils fileUtils;
 	
 	@Override
 	public List<BoardDto> selectBoardList() throws Exception {
@@ -20,13 +29,20 @@ public class BoardServiceImpl implements BoardService{
 	}
 	
 	@Override
-	public void insertBoard(BoardDto board) throws Exception {
+	public void insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception {
 		boardMapper.insertBoard(board);
+		List<BoardFileDto> list = fileUtils.parseFileInfo(board.getBoardIdx(), multipartHttpServletRequest);
+		if(CollectionUtils.isEmpty(list) == false){
+			boardMapper.insertBoardFileList(list);
+		}
 	}
 
 	@Override
 	public BoardDto selectBoardDetail(int boardIdx) throws Exception{
 		BoardDto board = boardMapper.selectBoardDetail(boardIdx);
+		List<BoardFileDto> fileList = boardMapper.selectBoardFileList(boardIdx);
+		board.setFileList(fileList);
+		
 		boardMapper.updateHitCount(boardIdx);
 		
 		return board;
@@ -40,6 +56,11 @@ public class BoardServiceImpl implements BoardService{
 	@Override
 	public void deleteBoard(int boardIdx) throws Exception {
 		boardMapper.deleteBoard(boardIdx);
+	}
+	
+	@Override
+	public BoardFileDto selectBoardFileInformation(int idx, int boardIdx) throws Exception {
+		return boardMapper.selectBoardFileInformation(idx, boardIdx);
 	}
 }	
 
---
+++ board/src/main/java/board/common/ExceptionHandler.java
@@ -0,0 +1,22 @@
+package board.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandler {
+	
+	@org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+	public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception exception){
+		ModelAndView mv = new ModelAndView("/error/error_default");
+		mv.addObject("exception", exception);
+		
+		log.error("defaultExceptionHandler 기본 예외 처리기", exception);
+		
+		return mv;
+	}
+}
